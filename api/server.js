f3a4372184766ccd31adab202d06e33e3a294e1d
Truecoder513 <truecoder513@gmail.com>
2022-10-04 15:43:30 +0100

    parametrage du server

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 
 const app = express();
 
+const path = require("path");
+
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +21,13 @@ app.use("/", links);
 port = process.env.NODE_ENV = "production"
   ? process.env.OUR_PORT
   : process.env.APP_PORT;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(process.cwd(), "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(process.cwd(), "client/build/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`${process.env.APP_NAME} listening on port ${port}`);
